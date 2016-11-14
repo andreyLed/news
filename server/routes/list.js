aa9748943c6dcfc8f261624e4c885898af37d829
sneg <sneg@example.com>
2016-11-14 15:28:49 +0200

    rout list init

--- server/routes/list.js
+++ server/routes/list.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const router = express.Router();
+const db = require('../news.json');
+
+function getIndex(arr, id) {
+  return arr.findIndex(item => item.id === Number(id));
+}
+router.get('/', (req, res) => {
+  res.status(200);
+  res.send(db.news);
+});
+
+router.get('/:id', (req, res) => {
+  const index = getIndex(db, req.param.id);
+  if (index === -1) {
+    res.status(404).send('404');
+  } else {
+    res.status(200);
+    res.json(db.cards[index]);
+  }
 });
 
 module.exports = router;
